9ece550f15f056baa30ecdf52678a7f15d648ac8
DE SOUZA GAGO JUAN IGNACIO <juani.desouza11@gmail.com>
2024-05-29 20:40:43 -0300

    Menu desplegable

--- app/src/main/java/ar/edu/ort/tp3/finaltp3/ui/_activities/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/tp3/finaltp3/ui/_activities/MainActivity.kt
@@ -2,20 +2,60 @@ package ar.edu.ort.tp3.finaltp3.ui._activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import ar.edu.ort.tp3.finaltp3.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavView : BottomNavigationView
     private lateinit var navHostFragment : NavHostFragment
+    private lateinit var  drawerLayout: DrawerLayout
+    private lateinit var  navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+
         bottomNavView = findViewById(R.id.bottom_bar)
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+
+        setupDrawerLayout()
+
+
+
     }
+
+    private fun setupDrawerLayout(){
+        val navController = navHostFragment.navController
+        navigationView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return false
+    }
+
+
 }
